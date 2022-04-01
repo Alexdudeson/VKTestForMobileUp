f6dc8958ad819e9c0933de7b1f96e52c6f5df31d
Alexey Yarov <alexxd91@gmail.com>
2022-04-02 00:03:43 +0300

    Initial Commit

---
+++ VKTestForMobileUp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  VKTestForMobileUp
+//
+//  Created by Alexey Yarov on 02.04.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
